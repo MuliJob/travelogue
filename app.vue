ac0fadd1455e3906fbea5bf4ea6fd44219946671
Muli Munyoki <149368775+MuliJob@users.noreply.github.com>
2025-05-07 00:26:34 +0300

    Tl10 (#49)
    * Fixed lint errors
    
    * created file association for tailwindcss
    
    * Fixed lint errors
    
    * DaisyUI is installed and configured
    
    * Dark mode support is added
    
    * Icons are installed and configured

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-emerald-500 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-outline btn-secondary">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
