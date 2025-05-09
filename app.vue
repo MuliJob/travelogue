dfb5d6a42b88d2c220be606ab1bffdafecd6e2cf
Muli Munyoki <149368775+MuliJob@users.noreply.github.com>
2025-05-09 20:58:08 +0300

    Added landing page (#51)

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-emerald-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-outline btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="hero min-h-screen"
+    style="background-image: url(https://worldlyadventurer.com/wp-content/uploads/2019/01/ethical-responsible-sustainable-tourism-1.jpg);"
+  >
+    <div class="hero-overlay" />
+    <div class="hero-content text-neutral-content text-center">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">
+          Travelogue
+        </h1>
+        <p class="mb-5">
+          Keep track of your travels and adventures with this simple travelogue app. Add locations, photos, and notes to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
