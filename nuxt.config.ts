7153786af57487426ca065f5ac9b8c3ef20cb86b
MuliJob <munyoki912@gmail.com>
2025-05-09 22:22:27 +0300

    :sparkles: add theme toggle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
   css: ["~/assets/css/main.css"],
   eslint: {
     config: {
@@ -14,5 +18,8 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss(),
     ],
+  },
+  colorMode: {
+    dataValue: "theme",
   },
 });
