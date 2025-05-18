74fc98844915d261f0781deae9416de6de6d0d4d
MuliJob <munyoki912@gmail.com>
2025-05-18 13:26:44 +0300

    :sparkles: Loading spinner displays during authentication | Successful login redirects to dashboard | Google icon shown on login button

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -2,18 +2,24 @@
 import { createAuthClient } from "better-auth/client";
 
 const authClient = createAuthClient();
+const loading = ref(false);
 
 async function signIn() {
+  loading.value = true;
   await authClient.signIn.social({
     provider: "google",
     callbackURL: "/dashboard",
   });
+  loading.value = false;
 }
 </script>
 
 <template>
-  <button class="btn btn-accent" @click="signIn">
-    Sign In With Google
-    <Icon name="tabler:brand-google-filled" size="24" />
+  <button :disabled="loading" class="btn btn-accent" @click="signIn">
+    <span v-if="loading" class="text-gray-950 loading loading-dots loading-md" />
+    <template v-else>
+      Sign In With Google
+      <Icon name="tabler:brand-google-filled" size="24" />
+    </template>
   </button>
 </template>
