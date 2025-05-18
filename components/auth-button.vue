5a93ecd02fcdcd2d8aa7947c9c66a14eb273b528
MuliJob <munyoki912@gmail.com>
2025-05-18 12:43:19 +0300

    :sparkles: Login with Google button initiates OAuth flow when clicked

---
+++ components/auth-button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+async function signIn() {
+  await authClient.signIn.social({
+    provider: "google",
+    callbackURL: "/dashboard",
+  });
+}
+</script>
+
+<template>
+  <button class="btn btn-accent" @click="signIn">
+    Sign In With Google
+    <Icon name="tabler:brand-google-filled" size="24" />
+  </button>
+</template>
