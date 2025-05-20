b28c9233b4bf61011d888503331ab4d93286a021
MuliJob <munyoki912@gmail.com>
2025-05-20 23:27:31 +0300

    :sparkles: Persistent collapsed/expanded state

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,20 @@
 <script lang="ts" setup>
 const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+});
+
+function toogleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
 </script>
 
 <template>
   <div class="flex-1 flex">
     <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
-      <div class="flex hover:cursor-pointer hover:bg-base-300 p-2" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="isSidebarOpen = !isSidebarOpen">
+      <div class="flex hover:cursor-pointer hover:bg-base-300 p-2" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="toogleSidebar">
         <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
         <Icon v-else name="tabler:chevron-right" size="32" />
       </div>
